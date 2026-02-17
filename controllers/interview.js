d8e563bdca3eaa7361d3aa161cc463f66222bc66
Nilesh <nilesh.netdeveloper@gmail.com>
2026-02-17 16:26:52 +0530

    crud api

--- controllers/interview.js
+++ controllers/interview.js
@@ -22,6 +22,79 @@ const insertData = async(req,res) => {
     }
 }
 
+
+const FetchData = async(req,res) => {
+    try {
+        const con = await Connection();
+
+        const { page, limit } = req.query;
+
+        const currentPage = Number(page) || 1;
+        const limitpage = Number(limit) || 3;
+        const offset = ( currentPage - 1 ) * limitpage;
+
+        const sql = `
+            select * from product limit ${limitpage} offset ${offset}
+        `;        
+
+        const [result] = await con.execute(sql,[
+            page,
+            limit
+        ]);
+
+        const [total] = await con.execute("select * from product");
+
+        return res.json({ message: "data fetch", status: true, data: result, totalpage: Math.ceil(total.length / limit) });
+    } catch (error) {
+        return res.json({message:"server error",status:false});
+    }
+}
+
+
+const editData = async(req,res) => {
+    try {
+        const con = await Connection();
+
+        const { product, price, stock } = req.body.ProductData;
+        const { editid } = req.body;
+
+        const sql = `
+            update product set product = ?, price = ?, stock = ? where id = ? 
+        `;
+
+        const [result] = await con.execute(sql,[
+            product,
+            price,
+            stock,
+            editid
+        ]);
+
+        return res.json({ message: "Update Successfully!", status: true });
+    } catch (error) {
+        return res.json({ message: error.message, status: false });
+    }
+}
+
+const deleteData = async(req,res) => {
+    try {
+        const con = await Connection();
+
+        const { id } = req.body;
+
+        const sql = `
+            delete from product where id = ?
+        `;
+
+        const [result] = await con.execute(sql,[
+            id
+        ]);
+
+        return res.json({ message: "deleted successfully!", status: true });
+    } catch (error) {
+        return res.json({ message: error.message, status: false });
+    }
+}
+
 module.exports = {
-    insertData
+    insertData, FetchData, editData, deleteData
 }
--- router/interview_route.js
+++ router/interview_route.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { insertData } = require('../controllers/interview')
+const { insertData, FetchData, editData, deleteData } = require('../controllers/interview')
 const interviewRoute = express.Router();
 
-interviewRoute.post("/insert",insertData);
+interviewRoute.post("/insert", insertData);
+interviewRoute.get("/getdata", FetchData);
+interviewRoute.put("/updatedata", editData);
+interviewRoute.delete("/deletedata", deleteData);
 
 module.exports = interviewRoute;
